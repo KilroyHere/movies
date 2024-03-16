c9689c243228fa8f52a97e1e9d76dbb35661374c
kilroyhere <aroo2001@gmail.com>
2024-03-15 18:36:02 -0700

    Range from 1940 through 2023

--- html/new.bat
+++ html/new.bat
@@ -1,5 +1,5 @@
 @echo off
-FOR /L %%G IN (1950,1,2023) DO (
+FOR /L %%G IN (1940,1,2023) DO (
     echo Downloading movie details for year %%G
     curl https://www.filmsite.org/%%G.html -o %%G.html
 )
